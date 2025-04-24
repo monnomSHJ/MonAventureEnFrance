f21d4e38362301f1b1db30854c36a211ec096b2f
monnomSHJ <149640003+monnomSHJ@users.noreply.github.com>
2025-04-24 10:07:17 +0900

    Delete Mon Aventure en France Prototype directory

--- Mon Aventure en France Prototype/data/scenes/intro2.js
+++
@@ -1,8 +0,0 @@
-export const intro2 = {
-    background_img: "../assets/images/airplane_window.jpg",
-    overlay_img: "",
-    lines: [
-        { speaker: "A", text: "안녕하세요"},
-        { speaker: "B", text: "알랄라라"}
-    ]
-};
