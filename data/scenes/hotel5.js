6b541ea3731f8b5667380cde8ae179769fa0b6d7
monnomSHJ <monnomshj22@gmail.com>
2025-05-07 12:20:26 +0900

    dd

--- data/scenes/hotel5.js
+++ data/scenes/hotel5.js
@@ -8,7 +8,15 @@ export function getHotel5Scene() {
         narration: "",
         retryLines: [],
         lines: [
-            { speaker: `👤 ${state.userName}`, text: `호텔 로비에 도착했다.` }
+            { speaker: `👤 ${state.userName}`, text: `일단 방에서 나오긴 했는데... 어디에 가야 할지를 모르겠네.` },
+            { speaker: `👤 ${state.userName}`, text: `아까 그 직원 분께 어디로 갈지 추천을 받아볼까?` },
+            { speaker: `👤 ${state.userName}`, text: `Excusez-moi.` },
+            { speaker: `👮 Réceptionniste`, text: `Oui, vous avez besoin d'aide ?`, personImg: "assets/images/hotelPerson1.png" },
+            { speaker: `👤 ${state.userName}`, text: `J'ai du mal à choisir quel endroit visiter en ce moment.`, personImg: "assets/images/hotelPerson1.png" },
+            { speaker: `👤 ${state.userName}`, text: `Connaissez-vous...`, personImg: "assets/images/hotelPerson1.png" },
+            { speaker: ``, text: `어떤 곳을 추천해달라고 부탁할까?`, personImg: "assets/images/hotelPerson1.png" },
+            
+            
         ],
         
         nextScene: () => {
